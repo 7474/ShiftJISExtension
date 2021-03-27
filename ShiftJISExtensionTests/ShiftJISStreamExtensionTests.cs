a0f46b90d6f984600a66ad29688ee44ac9373c0d
koudenpa <koudenpa@hotmail.com>
2021-03-28 02:12:21 +0900

    元がUTF-8だった場合は破壊しないつもりだったのでそうした。

--- ShiftJISExtensionTests/ShiftJISStreamExtensionTests.cs
+++ ShiftJISExtensionTests/ShiftJISStreamExtensionTests.cs
@@ -17,17 +17,31 @@ namespace ShiftJISExtension.Tests
         {
             var sjis = File.ReadAllBytes(@"Data/shift_jis.txt");
             var utf8 = File.ReadAllBytes(@"Data/utf8.txt");
+
             using (var sjisStream = new MemoryStream(sjis))
-            using (var utf8Stream = sjisStream.ToUTF8Async().Result)
+            using (var sjis2utf8Stream = sjisStream.ToUTF8Async().Result)
             using (var ms = new MemoryStream())
             {
-                utf8Stream.CopyTo(ms);
-                var sjis2utf = (ms.ToArray());
+                sjis2utf8Stream.CopyTo(ms);
+                var sjis2utf8 = (ms.ToArray());
                 var utf8Text = Encoding.UTF8.GetString(utf8);
-                var sjis2utf8Text = Encoding.UTF8.GetString(sjis2utf);
+                var sjis2utf8Text = Encoding.UTF8.GetString(sjis2utf8);
 
                 Assert.AreEqual(utf8Text, sjis2utf8Text);
-                Assert.IsTrue(utf8.SequenceEqual(sjis2utf));
+                Assert.IsTrue(utf8.SequenceEqual(sjis2utf8));
+            }
+
+            using (var utf8Stream = new MemoryStream(utf8))
+            using (var utf82utf8Stream = utf8Stream.ToUTF8Async().Result)
+            using (var ms = new MemoryStream())
+            {
+                utf82utf8Stream.CopyTo(ms);
+                var utf82utf8 = (ms.ToArray());
+                var utf8Text = Encoding.UTF8.GetString(utf8);
+                var utf82utf8Text = Encoding.UTF8.GetString(utf82utf8);
+
+                Assert.AreEqual(utf8Text, utf82utf8Text);
+                Assert.IsTrue(utf8.SequenceEqual(utf82utf8));
             }
         }
     }
