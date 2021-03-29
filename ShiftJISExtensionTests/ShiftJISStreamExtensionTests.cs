54ce9b915c12a9abfdb7ee3b95fc22ee657d7506
koudenpa <koudenpa@hotmail.com>
2021-03-30 00:56:34 +0900

    BOM付きUTF-8のテストケースを追加。
    BOMついていてもUTF-8とみなしていた。
    （stringに変換した際にBOMもBOM文字に変換される）

--- ShiftJISExtensionTests/ShiftJISStreamExtensionTests.cs
+++ ShiftJISExtensionTests/ShiftJISStreamExtensionTests.cs
@@ -17,31 +17,29 @@ namespace ShiftJISExtension.Tests
         {
             var sjis = File.ReadAllBytes(@"Data/shift_jis.txt");
             var utf8 = File.ReadAllBytes(@"Data/utf8.txt");
+            var utf8bom = File.ReadAllBytes(@"Data/utf8-bom.txt");
 
-            using (var sjisStream = new MemoryStream(sjis))
-            using (var sjis2utf8Stream = sjisStream.ToUTF8Async().Result)
-            using (var ms = new MemoryStream())
+            var cases = new[]
             {
-                sjis2utf8Stream.CopyTo(ms);
-                var sjis2utf8 = (ms.ToArray());
-                var utf8Text = Encoding.UTF8.GetString(utf8);
-                var sjis2utf8Text = Encoding.UTF8.GetString(sjis2utf8);
+                new { name = "sjis", input = sjis, expected = utf8, },
+                new { name = "utf8", input = utf8, expected = utf8,},
+                new { name = "utf8bom", input = utf8bom, expected = utf8bom,},
+            };
 
-                Assert.AreEqual(utf8Text, sjis2utf8Text);
-                Assert.IsTrue(utf8.SequenceEqual(sjis2utf8));
-            }
-
-            using (var utf8Stream = new MemoryStream(utf8))
-            using (var utf82utf8Stream = utf8Stream.ToUTF8Async().Result)
-            using (var ms = new MemoryStream())
+            foreach (var c in cases)
             {
-                utf82utf8Stream.CopyTo(ms);
-                var utf82utf8 = (ms.ToArray());
-                var utf8Text = Encoding.UTF8.GetString(utf8);
-                var utf82utf8Text = Encoding.UTF8.GetString(utf82utf8);
+                using (var inputStream = new MemoryStream(c.input))
+                using (var outputStream = inputStream.ToUTF8Async().Result)
+                using (var ms = new MemoryStream())
+                {
+                    outputStream.CopyTo(ms);
+                    var actual = (ms.ToArray());
+                    var utf8Text = Encoding.UTF8.GetString(c.expected);
+                    var actualText = Encoding.UTF8.GetString(actual);
 
-                Assert.AreEqual(utf8Text, utf82utf8Text);
-                Assert.IsTrue(utf8.SequenceEqual(utf82utf8));
+                    Assert.AreEqual(utf8Text, actualText, $"{c.name} text equals");
+                    Assert.IsTrue(c.expected.SequenceEqual(actual), $"{c.name} sequence equals");
+                }
             }
         }
     }
